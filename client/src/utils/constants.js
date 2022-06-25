3caeb7ad4988c9cf618a9247818509771c9d6cfb
@rajat1106 <rajatharne@gmail.com>
2022-06-25 08:52:23 +0530

    Added my project

---
+++ client/src/utils/constants.js
@@ -0,0 +1,5 @@
+
+import abi from './Transactions.json';
+
+export const contractABI = abi.abi;
+export const contractAddress = '0xb443ca53bd175e9E32E8eA52aCD16424CE3cD2c6';
---
+++ smart_contract/hardhat.config.js
@@ -0,0 +1,11 @@
+require('@nomiclabs/hardhat-waffle');
+
+module.exports = {
+  solidity: '0.8.0',
+  networks: {
+    goerli: {
+      url: 'https://eth-goerli.alchemyapi.io/v2/cN2b4pXlDNWo0dIXrupCT_kThmiHnyVS',
+      accounts: [ '8aea0386f167358190238be32e348082bd703c52f0f7b54a1517797ae3a9e349' ]
+    }
+  }
+}
